492436a7bb65812f66d4cfff0f7a070163c0c770
Jannix <72571556+jjannix@users.noreply.github.com>
2024-06-25 20:36:58 +0200

    added files

---
+++ public/app.js
@@ -0,0 +1,16 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(reg => {
+            console.log('Service worker registered!', reg);
+        })
+        .catch(err => {
+            console.log('Service worker registration failed:', err);
+        });
+}
+
+const socket = io();
+
+function transition(sceneName) {
+    socket.emit('transition', { sceneName });
+}
+    
